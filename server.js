84a0a54c2b76c759aa3dd5e90c34de10490a5b9c
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2026-01-30 23:53:35 +0200

    Starting point of my blog app.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const userRouter = require("./routers/userRouter")
+const connectDB = require("./mongodb/dbconnection")
+const cookieParser = require("cookie-parser")
+
+require('dotenv').config()
+connectDB()
+
+const server = express()
+const port = process.env.PORT 
+
+// middlewares
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+server.use(cookieParser())
+
+server.use('/api', userRouter)
+
+server.listen(port, () => {
+    console.log(`Server is listening to port ${port}`)
+})
