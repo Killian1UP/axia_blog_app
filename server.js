94f98e10cf738428ac6eb9b084042d7b2a0019cc
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2026-02-04 19:09:46 +0200

    I modified the server to use the postRouter and authRouter.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const userRouter = require("./routers/userRouter")
 const connectDB = require("./mongodb/dbconnection")
 const cookieParser = require("cookie-parser")
+const postRouter = require("./routers/postRouter")
+const authRouter = require("./routers/authRouter")
 
 require('dotenv').config()
 connectDB()
@@ -15,6 +17,8 @@ server.use(express.urlencoded({extended: true}))
 server.use(cookieParser())
 
 server.use('/api', userRouter)
+server.use('/api', postRouter)
+server.use('/api', authRouter)
 
 server.listen(port, () => {
     console.log(`Server is listening to port ${port}`)
